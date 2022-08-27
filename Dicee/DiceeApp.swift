2b76fae41a530ef3bcc462a520aff1e9328e7879
Emmanuel George <Emmanuelgeorge1000@gmail.com>
2022-08-28 00:55:54 +0530

    Initial Commit

---
+++ Dicee/DiceeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceeApp.swift
+//  Dicee
+//
+//  Created by Emmanuel George on 28/08/22.
+//
+
+import SwiftUI
+
+@main
+struct DiceeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
